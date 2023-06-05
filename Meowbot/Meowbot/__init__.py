8e0dfda208e6c57e3013e495117bb03782885205
waylon.yuen <waylon.yuen@gosmio.biz>
2023-06-05 18:30:01 +0800

    Migration

---
+++ Meowbot/Meowbot/__init__.py
@@ -0,0 +1,18 @@
+﻿"""
+The flask application package.
+"""
+
+from Meolask import app
+
+
+# 引入
+from Meowbot.Views.LineBotView import LineBot_View as v_LineBot
+
+
+# Config
+# -----------------------------------------------------
+conf_dbgMode = True
+# -----------------------------------------------------
+
+# 注冊-路由
+app.register_view(v_LineBot,             '/api/lineBot/view', conf_dbgMode)           # 注冊-視圖
---
+++ Meowbot/program.py
@@ -0,0 +1,27 @@
+﻿# from Program
+"""
+This script runs the BotService application using a development server.
+"""
+import configparser
+from Meolask import app
+import Meowbot
+
+# 設定檔
+config = configparser.ConfigParser()
+config.read('settings.ini') # 讀取-設定檔
+#config.get('欄位', '變數名稱')
+
+# 主程式
+if __name__ == '__main__':
+
+    HOST = config.get('Service', 'Host')
+    PORT = config.get('Service', 'Port')
+
+    app.run(HOST, PORT)
+
+
+
+## Tmp
+#from os import environ
+#HOST = environ.get('SERVER_HOST', 'localhost')
+#PORT = environ.get('SERVER_PORT', '5555')
